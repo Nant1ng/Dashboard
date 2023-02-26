90648e2e89bd4b596ffa2789379e786de32f33b8
Andrés <andressantana99@hotmail.se>
2023-02-26 17:00:02 +0100

    Added a Navbar and some Style

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>Dashboard</h1>
+      <div className="links">
+        <Link to="/" style={{ color: "rgb(77, 77, 77)" }}>
+          Home
+        </Link>
+        <Link
+          to="/create"
+          style={{
+            color: "white",
+            backgroundColor: "rgb(77, 77, 77)",
+            borderRadius: "0.5rem",
+          }}
+        >
+          New
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
